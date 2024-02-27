6f2e9cbfec52f456749c00555535708977ff1326
Yanis Bouchilloux <74117327+Sans-Interdit@users.noreply.github.com>
2024-02-27 17:58:28 +0100

    Delete tft-match-viewer/java/com/riotgames/tftanalytics directory

--- tft-match-viewer/java/com/riotgames/tftanalytics/controller/Main.java
+++
@@ -1,7 +0,0 @@
-package com.riotgames.tftanalytics.controller;
-
-public class Main {
-	public static void main(String[] args) {
-		new RiotAPI().getMatchInfos(new RiotAPI().getMatchIds(new RiotAPI().getPuuid("clé","euw")).get(0));
-	}
-}
